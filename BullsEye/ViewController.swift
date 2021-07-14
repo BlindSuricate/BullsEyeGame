28afa41d8d1f88ccb33671a9944f1ee5a4e2a1fd
Сергей Тарасенко <blindsuricate@gmail.com>
2021-07-14 15:37:17 +0300

    Merge Dev into Master

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         print("Try to clone and understand how its work")
+        
+        print("Merge Dev Into Master")
     }
 }
 
